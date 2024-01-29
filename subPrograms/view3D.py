13f8fa3f7c254722a696838146d610559e02d988
amirhossein-mlkz <amirhossein.mlkz@gmail.com>
2024-01-29 14:05:56 +0330

    depth image to xyz added

--- subPrograms/view3D.py
+++ subPrograms/view3D.py
@@ -28,17 +28,27 @@ class view3D:
                                   up=[-0.0694, -0.9768, 0.2024])
 
 
+def depthImage2xyz(img:np.ndarray):
+    xy = list(np.ndindex(img.shape[1], img.shape[0]))
+    xy = np.array(xy)
+    z = img[xy[:,1], xy[:,0]]
+    z = np.expand_dims(z, axis=1)
+    xyz = np.hstack((xy,z))
+    return xyz
 
 
 if __name__ == "__main__":
-    x = np.linspace(-3, 3, 1000)
-    mesh_x, mesh_y = np.meshgrid(x, x)
-    z = np.sinc((np.power(mesh_x, 2) + np.power(mesh_y, 2)))
-    z_norm = (z - z.min()) / (z.max() - z.min())
-    xyz = np.zeros((np.size(mesh_x), 3))
-    xyz[:, 0] = np.reshape(mesh_x, -1)
-    xyz[:, 1] = np.reshape(mesh_y, -1)
-    xyz[:, 2] = np.reshape(z_norm, -1)
+
+    img = np.random.rand(640,1000)
+    xyz = depthImage2xyz(img)
+    # x = np.linspace(-3, 3, 1000)
+    # mesh_x, mesh_y = np.meshgrid(x, x)
+    # z = np.sinc((np.power(mesh_x, 2) + np.power(mesh_y, 2)))
+    # z_norm = (z - z.min()) / (z.max() - z.min())
+    # xyz = np.zeros((np.size(mesh_x), 3))
+    # xyz[:, 0] = np.reshape(mesh_x, -1)
+    # xyz[:, 1] = np.reshape(mesh_y, -1)
+    # xyz[:, 2] = np.reshape(z_norm, -1)
 
 
     v3 = view3D()
